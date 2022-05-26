c94df0544bb6b350ff1af8f430c3ea1da08113cc
armin-bkh <arminbkh0921@yahoo.com>
2022-05-27 04:00:03 +0430

    services section + Service component

---
+++ components/Service/Service.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface ServicePropsType {
+  service: {
+    title: string;
+    description: string;
+    theme: string;
+  };
+  ServiceIcon: React.ComponentType;
+}
+
+const Service = (props: ServicePropsType) => {
+  const { service, ServiceIcon } = props;
+  return (
+    <div className="mt-10 first:mt-0 sm:mt-0 shadow-lg even:shadow-sky-400/40 odd:shadow-violet-400/40 rounded-[4rem] w-56 h-56 p-8">
+      <div
+        className={`rounded-full ${service.theme} text-white w-16 h-16 text-2xl flex justify-center items-center`}
+      >
+        <ServiceIcon />
+      </div>
+      <h5 className="mt-5 mb-2 font-bold">{service.title}</h5>
+      <p className="text-gray-400 text-sm">{service.description}</p>
+    </div>
+  );
+};
+
+export default Service;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,40 @@ import type { NextPage } from "next";
 import styles from "styles/home.module.css";
 import Head from "next/head";
 import Link from "next/link";
+import { MdOutlineDeliveryDining, MdMoreTime } from "react-icons/md";
+import { FaBoxOpen, FaShoppingCart } from "react-icons/fa";
+import Service from "components/Service/Service";
+
+const services = [
+  {
+    id: 1,
+    title: "Free Shipping",
+    description: "we delivery free shipping world wide",
+    icon: MdOutlineDeliveryDining,
+    theme: "bg-[#2563eb]",
+  },
+  {
+    id: 2,
+    title: "Support 24/8",
+    description: "we support our customers 24 hours",
+    icon: MdMoreTime,
+    theme: "bg-[#f43f5e]",
+  },
+  {
+    id: 3,
+    title: "Returns",
+    description: "we can return our product after 7 days",
+    icon: FaBoxOpen,
+    theme: "bg-[#f43f5e]",
+  },
+  {
+    id: 4,
+    title: "Track Order",
+    description: "you can track product in online",
+    icon: FaShoppingCart,
+    theme: "bg-[#f97316]",
+  },
+];
 
 const HomePage: NextPage = () => {
   return (
@@ -25,6 +59,15 @@ const HomePage: NextPage = () => {
               </a>
             </Link>
           </div>
+        </section>
+        <section className="flex flex-col sm:flex-row flex-wrap justify-evenly py-10 items-center safeArea">
+          {services.map((service) => (
+            <Service
+              key={service.id}
+              service={service}
+              ServiceIcon={service.icon}
+            />
+          ))}
         </section>
       </main>
     </>
